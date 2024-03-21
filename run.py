b1b7f54e03f2202068c8b8a1d56630aad5cca01a
Dabobroto Sarkar <35169303+dstushar7@users.noreply.github.com>
2024-03-21 22:20:25 +0600

    Structurizing the whole project
    Making the full readable project structure

--- run.py
+++ run.py
@@ -3,8 +3,13 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 
 @app.route('/')
-def read_root():
+def index():
     return jsonify({"message": "Hello, World!"})
+
+
+@app.route('/home')
+def home():
+    return jsonify({"message": "This is the homepage!"})
 
 if __name__ == '__main__':
     app.run(debug=True)
